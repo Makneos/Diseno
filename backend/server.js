688bf79a6194b5b9440ad05b09430c5871ac9646
Makneos <167037589+Makneos@users.noreply.github.com>
2025-11-19 21:21:29 -0300

    dependencias

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 5000;
 const DB_CONFIG = {
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'Farmacia?#2027',
+  password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'farmacia',
   port: process.env.DB_PORT || 3306,
   waitForConnections: true,
